6cdf62c9a90d76ed0d70d300586946e61b8121ba
Geice Sousa Pinho <geicesousa@outlook.com>
2023-07-12 14:42:09 -0300

    alteração do caminho para o vercel

--- api/server.js
+++ api/server.js
@@ -6,15 +6,15 @@ const middlewares = jsonServer.defaults()
 
 const app = jsonServer.create();
 
-const data = fs.readFileSync(path.resolve(__dirname, '../db.json'), 'utf8');
-fs.writeFileSync('db.json', data);
-
-const router = jsonServer.router('db.json'); // para windows
-
 // const data = fs.readFileSync(path.resolve(__dirname, '../db.json'), 'utf8');
-// fs.writeFileSync('/tmp/db.json', data);
+// fs.writeFileSync('db.json', data);
 
-// const router = jsonServer.router('/tmp/db.json'); // para o vercel que funciona como linux com \
+// const router = jsonServer.router('db.json'); // para windows
+
+const data = fs.readFileSync(path.resolve(__dirname, '../db.json'), 'utf8');
+fs.writeFileSync('/tmp/db.json', data);
+
+const router = jsonServer.router('/tmp/db.json'); // para o vercel que funciona como linux com \
 
 const port = process.env.PORT || 8080;
 
