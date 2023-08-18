7b9319aeb4d74fb53d00a45dffc486c1a0b20c35
Geice Sousa Pinho <geicesousa@outlook.com>
2023-08-18 10:08:18 -0300

    config vercel

--- api/server.js
+++ api/server.js
@@ -4,17 +4,17 @@ const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 const middlewares = jsonServer.defaults()
 
-const app = jsonServer.create();
-
-const data = fs.readFileSync(path.resolve(__dirname, '../db.json'), 'utf8');
-fs.writeFileSync('db.json', data);
-
-const router = jsonServer.router('db.json'); // para windows
+// const app = jsonServer.create();
 
 // const data = fs.readFileSync(path.resolve(__dirname, '../db.json'), 'utf8');
-// fs.writeFileSync('/tmp/db.json', data);
+// fs.writeFileSync('db.json', data);
 
-// const router = jsonServer.router('/tmp/db.json'); // para o vercel que funciona como linux com \
+// const router = jsonServer.router('db.json'); // para windows
+
+const data = fs.readFileSync(path.resolve(__dirname, '../db.json'), 'utf8');
+fs.writeFileSync('/tmp/db.json', data);
+
+const router = jsonServer.router('/tmp/db.json'); // para o vercel que funciona como linux com \
 
 const port = process.env.PORT || 3000;
 
